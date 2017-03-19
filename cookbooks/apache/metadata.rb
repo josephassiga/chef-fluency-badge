d748ab7d77b500f04bbff53e48ffd96e0e9f7726
josephassiga <joseph.assiga@gmail.com>
2017-03-19 00:14:20 +0000

    Updade Apache

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'joseph.assiga@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '2.0.0'
--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -5,5 +5,5 @@ end
 
 file 'default wwww' do
   path '/var/www/html/index.html'
-  content 'Hello Chef With Joseph'
+  content 'Hello Chef With Joseph v2.0'
 end 
